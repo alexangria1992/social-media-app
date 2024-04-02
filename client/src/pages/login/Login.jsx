cb856754d4aea65025626e04d8cd4239388c4ecf
Alexandre Angrignon <alex040892@gmail.com>
2024-04-02 15:26:30 +1300

    Login Page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import "./login.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            excepturi voluptate laborum cupiditate, dolore libero! Fugiat
+            ratione hic cumque ex?
+          </p>
+          <span>Don't have an account?</span>
+          <button>Register</button>
+        </div>
+
+        <div className="right">
+          <h1>Login</h1>
+          <form action="">
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
