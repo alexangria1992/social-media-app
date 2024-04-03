8a9ccbba1e610c35726230fde3e22e5f1c7de043
Alexandre Angrignon <alex040892@gmail.com>
2024-04-03 15:15:13 +1300

    Registering Users to Database Successfully

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,15 +1,33 @@
 import { db } from "../connect.js";
+import bcrypt from "bcryptjs";
 
 export const register = (req, res) => {
   //Check user if exists
-  const q = "SELECT FROM users WHERE username = ?";
+  const q = "SELECT * FROM users WHERE username = ?";
 
   db.query(q, [req.body.username], (err, data) => {
     if (err) return res.status(500).json(err);
-    if (data.length) return res.status(409).json("user already exists!");
+    if (data.length) return res.status(409).json("User already exists!");
+    //CREATE A NEW USER
+    //Hash the password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+    const q =
+      "INSERT INTO users (`username`,`email`,`password`,`name`) VALUE (?)";
+
+    const values = [
+      req.body.username,
+      req.body.email,
+      hashedPassword,
+      req.body.name,
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("User has been created.");
+    });
   });
-  //CREATE A NEW USER
-  //HAsh the password
 };
 export const login = (req, res) => {};
 
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
 import express from "express";
+const app = express();
+
 import colors from "colors";
 import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 import commentRoutes from "./routes/comments.js";
 import likeRoutes from "./routes/likes.js";
-const app = express();
 
 //middlewares
-app.use(express.json);
+app.use(express.json());
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
