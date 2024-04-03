9d4cd90b2785217a10ab7c104bd72764f06648ad
Alexandre Angrignon <alex040892@gmail.com>
2024-04-03 15:45:03 +1300

    Successful Login with correct error handling

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,6 @@
 import { db } from "../connect.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
   //Check user if exists
@@ -29,6 +30,33 @@ export const register = (req, res) => {
     });
   });
 };
-export const login = (req, res) => {};
+
+export const login = (req, res) => {
+  const q = "SELECT * FROM users WHERE username = ?";
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length === 0) return res.status(404).json("User not found!");
+    const checkPassword = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+    if (!checkPassword)
+      return res.status(400).json("Wrong Password or username");
+
+    //JSONWEBTOKEN
+    const token = jwt.sign({ id: data[0].id }, "secretkey");
+
+    const { password, ...others } = data[0];
+
+    //Destructure User
+
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
+  });
+};
 
 export const logout = (req, res) => {};
--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,13 @@ import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 import commentRoutes from "./routes/comments.js";
 import likeRoutes from "./routes/likes.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 //middlewares
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
